dcae076882ab7d06a4daec84248b17cebd2520e1
Білецька <khrista.biletcka7@gmail.com>
2026-04-29 23:13:03 +0300

    init

---
+++ src/js/render-functions.js
@@ -0,0 +1,40 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images.map(img => `
+    <li class="gallery-item">
+      <a href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" alt="${img.tags}" />
+      </a>
+      <div class="info">
+        <p><b>Likes</b>${img.likes}</p>
+        <p><b>Views</b>${img.views}</p>
+        <p><b>Comments</b>${img.comments}</p>
+        <p><b>Downloads</b>${img.downloads}</p>
+      </div>
+    </li>
+  `).join("");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+export function showLoader() {
+  document.querySelector(".loader").classList.add("active");
+}
+
+export function hideLoader() {
+  document.querySelector(".loader").classList.remove("active");
+}
---
+++ src/main.js
@@ -0,0 +1,55 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from "./js/pixabay-api";
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader
+} from "./js/render-functions";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const query = e.target.elements["search-text"].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      message: "Please fill in the field!",
+      position: "topRight",
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message: "Sorry, there are no images matching your search query. Please try again!",
+          position: "topRight",
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: "Something went wrong!",
+        position: "topRight",
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+
+  form.reset();
+}
